1b24c863cd97aa4e017d72effa89f07bd2ff8a6a
Akash Masney <53115722+akashmasne@users.noreply.github.com>
2021-10-10 02:37:21 +0530

    Update fetch_top_stories_eu_parliament.py

--- fetch_top_stories_eu_parliament.py
+++ fetch_top_stories_eu_parliament.py
@@ -1,12 +1,15 @@
-#Libs required for the task
 import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
+import argparse
+import logging
+import os
 
 #Below variables can be also be taken as user input/arguments OR from a config file as required.
-url = 'https://www.europarl.europa.eu/rss/doc/top-stories/en.xml'
+#url = 'https://www.europarl.europa.eu/rss/doc/top-stories/en.xml'
+#output_csv_filename='eu-rss.csv'
 xml_data_filename='eu-topstories.xml'
-output_csv_filename='eu-rss.csv'
+
 
 def parseXML(xmlfile):
     # create element tree object
@@ -23,18 +26,39 @@ def parseXML(xmlfile):
         # return list of all the news items, where each element is dict for a news item
     return newsitems
 
-def main():
-    # error handling
-    try:
-        resp = requests.get(url)
-        #Storing xml response in local temporarily,can be deleted after use
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+      '--url',
+      dest='url',
+      default='https://www.europarl.europa.eu/rss/doc/top-stories/en.xml',
+      help='RSS endpoint for top stories')
+      
+    parser.add_argument(
+      '--output_csv_filename',
+      dest='output_csv_filename',
+      default='eu-rss.csv',
+      help='output_csv_filename for ouput')
+      
+    #known_args, other_args = parser.parse_known_args(argv)
+    known_args=parser.parse_args()
+    rssurl=known_args.url
+    rss_output_csv_filename=known_args.output_csv_filename
+
+
+    try:  # error handling
+        resp = requests.get(rssurl)
         with open(xml_data_filename, 'wb') as f:
             f.write(resp.content)
         newsitems = parseXML(xml_data_filename)
         df=pd.DataFrame(newsitems)
-        df.to_csv(output_csv_filename)
+        df.to_csv(rss_output_csv_filename)
+
+        os.remove(xml_data_filename)
+
     except Exception as e: 
         print("Error:",str(e))
 
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
     main()
